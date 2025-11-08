450d93cb61055708538205f09101b9a3529fda0b
Sahil Chaddha <mail@sahilchaddha.com>
2025-11-08 02:21:16 -0500

    Update linux.sh

--- client/linux.sh
+++ client/linux.sh
@@ -13,12 +13,14 @@ for i in "${!INET[@]}"; do
 done
 IP_JSON=${IP_JSON%,} # Remove trailing comma
 IP_JSON+="}"
-
+EXTERNAL_IP=$(curl -s http://checkip.dyndns.org | sed 's/[^0-9.]//g')
+echo $EXTERNAL_IP
 # Build the JSON payload using heredoc for proper formatting
 JSON_PAYLOAD=$(cat <<EOF
 {
 	"name": "$CLIENT_NAME",
 	"local_ip": "$LOCAL_IP",
+	"external_ip": "$EXTERNAL_IP",
 	"hostname": "$LOCAL_HOSTNAME.local",
 	"custom_tags": {
 		"location": "office"
